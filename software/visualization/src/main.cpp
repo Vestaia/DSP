6edc413e197a2fad1b34b50c9ddab48925519005
Lars Hebenstiel <lars.hebenstiel@gmail.com>
2021-06-24 18:58:52 -0400

    Set up visualization, remove glfw lib from repo

--- software/visualization/src/main.cpp
+++ software/visualization/src/main.cpp
@@ -498,7 +498,9 @@ int main(int, char**)
             static float f = 0.0f;
             static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Hello, world!");            
+            
+            ImGui::SetWindowPos(ImVec2(0, 0));              // Create a window called "Hello, world!" and append into it.
 
             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
             ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
