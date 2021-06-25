eb7e7e403224805347168d0e97d82d8d4de72a68
vestaia <ywa329@uky.edu>
2021-06-25 19:42:53 -0400

    Make makefiles great again

--- software/build/release/objs/libs/imgui/src/imgui_demo.d
+++
@@ -1,7 +0,0 @@
-/Users/koko/git/DSP/software/build/release/objs/libs/imgui/src/imgui_demo.o: \
-  libs/imgui/src/imgui_demo.cpp libs/imgui/include/imgui.h \
-  libs/imgui/include/imconfig.h
-
-libs/imgui/include/imgui.h:
-
-libs/imgui/include/imconfig.h:
--- software/visualization/README.md
+++
@@ -1,6 +0,0 @@
-Dependencies:
-
-- GLFW3
-- Vulkan
-
-To compile the code, run `make release --jobs=n` where `n` is the number of compilation threads to run in parallel.
