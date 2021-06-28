c93f2b2a3532742fc7df9fa5ab96c2f629889755
Lars Hebenstiel <lars.hebenstiel@gmail.com>
2021-06-28 11:28:46 -0400

    Refactoring

---
+++ software/dev/oscilloscope/libs/fpga/common.h
@@ -0,0 +1,72 @@
+#pragma once
+#define CONFIG_ADDR 0x40000000
+#define CLOCK_FREQ 125000000
+template <class fpga_cfg, class sample>
+class fpga {
+
+    public:
+    int fd;
+    volatile uint32_t *slcr, *hp0;
+    volatile fpga_cfg* cfg;
+    volatile uint8_t* ring_buf;
+    fpga();
+    ~fpga();
+
+    //Obtain n raw samples starting now
+    void capture_n_raw(sample* data, unsigned int n);
+    
+    //Obtain raw samples for t seconds
+    void capture_t_raw(sample* data, float t);
+
+    //Obtain n events starting now
+    void capture_n_events(sample* data, unsigned int n);
+
+    //Obtain events for t seconds
+    void capture_t_events(sample* data, float t);
+
+    int reset();
+};
+
+template <class fpga_cfg, class sample>
+fpga<fpga_cfg,sample>::fpga(){
+    if((fd = open("/dev/mem", O_RDWR)) < 0){
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+    slcr = (volatile uint32_t*) mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0xF8000000);
+    hp0  = (volatile uint32_t*) mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0xF8008000);
+    cfg  = (volatile fpga_cfg*) mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, CONFIG_ADDR);
+    slcr[2] = 0xDF0D;
+    slcr[144] = 0;
+    hp0[0] &= ~1;
+    hp0[5] &= ~1;
+}
+
+template <class fpga_cfg, class sample>
+fpga<fpga_cfg,sample>::~fpga(){
+    munmap((void*)slcr, sysconf(_SC_PAGESIZE));
+    munmap((void*)hp0, sysconf(_SC_PAGESIZE));
+    munmap((void*)cfg, sysconf(_SC_PAGESIZE));
+    close(fd);
+}
+
+//TODO: Make this actually work std::memcpy()
+template <class fpga_cfg, class sample>
+void fpga<fpga_cfg,sample>::capture_n_raw(sample* data, unsigned int n){
+    for (int i = 0; i < n; i++)
+        data[i] = *((sample *)(ring_buf + sizeof(sample) * i));
+}
+template <class fpga_cfg, class sample>
+void fpga<fpga_cfg,sample>::capture_t_raw(sample* data, float t){
+    fpga::capture_n_raw(data, t * CLOCK_FREQ);
+}
+
+//TODO: Implement this
+template <class fpga_cfg, class sample>
+void fpga<fpga_cfg,sample>::capture_n_events(sample* data, unsigned int n){
+}
+
+template <class fpga_cfg, class sample>
+void fpga<fpga_cfg,sample>::capture_t_events(sample* data, float t){
+    fpga::capture_n_events(data, t * CLOCK_FREQ);
+}
---
+++ software/dev/oscilloscope/libs/fpga/piecewise/fpga_driver.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <math.h>
+#include "common.h"
+
+#define DATA_RING_ADDR 0x10000000
+#define DATA_RING_SIZE (1<<25)
+#define RESET_WRITER (1<<2)
+#define RESET_DAQ (1<<1)
+#define PIECES 2
+#define ORDER 3
+
+//Configuration Register Structure
+#pragma pack(push, 1) //Disable padding
+struct fpga_cfg {                     
+    volatile uint8_t  reset;                        //0x00
+    volatile uint32_t coef[PIECES * (ORDER + 1)];   //0x01
+    volatile uint16_t delay[PIECES - 1];
+};
+#pragma pack(pop)
+
+//Structure of single sample from DAQ
+#pragma pack(push, 1)
+struct sample {
+    volatile uint32_t time;
+    volatile int16_t  ch_a;
+    volatile int16_t  ch_b;
+};
+#pragma pack(pop)
+
+class pwp_fpga : public fpga<fpga_cfg, sample> {
+    
+    public:
+    pwp_fpga();
+    ~pwp_fpga();
+
+    //Set coefficients of polynomial filter
+    int set_coef(uint32_t *coef);
+
+    //Set coefficients using polynomial coeffients
+    int set_poly_coef(uint32_t *coef);
+
+    int reset();
+};
